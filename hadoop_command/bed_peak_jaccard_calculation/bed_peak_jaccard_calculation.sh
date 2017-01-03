7c97d06873d6b2fba9e13ff67d8d47c26cf2765f
avik <avikd@ebi.ac.uk>
2017-01-03 11:10:18 +0530

    Update bed_peak_jaccard_calculation.sh

--- hadoop_command/bed_peak_jaccard_calculation/bed_peak_jaccard_calculation.sh
+++ hadoop_command/bed_peak_jaccard_calculation/bed_peak_jaccard_calculation.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 yarn jar /PATH/hadoop-2.7.3/share/hadoop/tools/lib/hadoop-streaming-2.7.3.jar \
  -D mapred.reduce.tasks=0 \
  -mapper /PATH/git/hadoop_streaming_python_script/hadoop_mapper_wrapper/bed_peak_jaccard_calculation_mapper.sh \
