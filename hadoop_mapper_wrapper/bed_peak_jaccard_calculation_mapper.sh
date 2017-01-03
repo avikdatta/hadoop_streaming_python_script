d8ead9b4540306893e5b9dc6337d3a877515f241
avik <avikd@ebi.ac.uk>
2017-01-03 11:06:16 +0530

    Update bed_peak_jaccard_calculation_mapper.sh

--- hadoop_mapper_wrapper/bed_peak_jaccard_calculation_mapper.sh
+++ hadoop_mapper_wrapper/bed_peak_jaccard_calculation_mapper.sh
@@ -1,1 +1,5 @@
-/usr/bin/env docker run -i -v /PATH/git/hadoop_streaming_python_script:/app  avikdatta/python_data:latest bash -c 'export PATH=$PATH:/root/bedtools2/bin; python /app/script/bed_peak_jaccard_calculation/bed_peak_mapper.py -f FILE_PREFIX -H MYSQL_HOST -n MYSQL_DBNAME -u MYSQL_USER -p MYSQL_PASS'
+#!/bin/bash
+/usr/bin/env docker run -i                                 \
+         -v /PATH/git/hadoop_streaming_python_script:/app  \
+         avikdatta/python_data:latest                      \
+         bash -c 'export PATH=$PATH:/root/bedtools2/bin; python /app/script/bed_peak_jaccard_calculation/bed_peak_mapper.py -f FILE_PREFIX -H MYSQL_HOST -n MYSQL_DBNAME -u MYSQL_USER -p MYSQL_PASS'
